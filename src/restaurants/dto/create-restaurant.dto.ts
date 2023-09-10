1a2154759c44dc67b43ed4ccd12c69598acb6ae4
kotekirkitadze <kotekirkitadze@gmail.com>
2023-09-10 19:10:03 +0400

    add parameter definition for swagger

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,10 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Category } from '../schemas/restaurant.schema';
 
 export class CreateRestaurantDto {
+  @ApiProperty({
+    description: 'The name of the restaurant',
+    example: 'Test Restaurant',
+  })
   readonly name: string;
+  @ApiProperty({
+    description: 'The description of the restaurant',
+    example: 'Test description',
+  })
   readonly description: string;
+  @ApiProperty({
+    description: 'The email of the restaurant',
+    example: 'test@gmail.com',
+  })
   readonly email: string;
+  @ApiProperty()
   readonly phoneNo: number;
+  @ApiProperty()
   readonly address: string;
+  @ApiProperty({
+    enum: ['Fast Food', 'Cafe', 'Fine Dinning'],
+    enumName: 'Category',
+    description: 'The category of the restaurant',
+    example: 'Cafe',
+  })
   readonly category: Category;
 }
