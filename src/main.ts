42970669f1402081ba1dbe63e63cee286430b07e
kotekirkitadze <kotekirkitadze@gmail.com>
2023-09-11 01:06:57 +0400

    install packages for pipes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Restaurants example')
     .setDescription('The restaurants API description')
     .setVersion('1.0')
-
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
+
+  // app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
