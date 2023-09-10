8da59636d87698d1fddbeafb564fb8ef96903d62
kotekirkitadze <kotekirkitadze@gmail.com>
2023-09-10 18:36:27 +0400

    add api property decorators

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,7 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Category } from '../schemas/restaurant.schema';
 
 export class UpdateRestaurantDto {
+  @ApiProperty({
+    description: 'The name of the restaurant',
+    example: 'Restaurant 1',
+  })
   readonly name: string;
+
+  @ApiProperty({
+    description: 'The description of the restaurant',
+    example: 'description 1',
+  })
   readonly description: string;
   readonly email: string;
   readonly phoneNo: number;
